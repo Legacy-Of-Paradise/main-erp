f8ef2b84250432c26da4ce689bafa11c0672c5cd
Evgencheg <7064926@gmail.com>
2025-04-25 00:32:57 +0300

    Попытка не пытка?

---
+++ Content.Server.Database/Migrations/Sqlite/20250424213216_TTS.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class TTS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "voice",
+                table: "profile",
+                newName: "voice_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "voice_id",
+                table: "profile",
+                newName: "voice");
+        }
+    }
+}
