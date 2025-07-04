89ea793a322a65384ab3fd0132c6a3f78ae25d9a
Evgencheg <73418250+Evgencheg@users.noreply.github.com.>
2025-07-05 01:49:45 +0300

    Автоматическое голосование

--- Content.Shared/_EE/CCVar/EECCVars.cs
+++ Content.Shared/_EE/CCVar/EECCVars.cs
@@ -186,4 +186,22 @@ public sealed partial class EECCVars : CVars
         CVarDef.Create("supermatter.yell_timer", 60f, CVar.SERVER);
 
     #endregion
+
+    #region autovote
+
+    /// Enables the automatic voting system.
+    public static readonly CVarDef<bool> AutoVoteEnabled =
+        CVarDef.Create("vote.autovote_enabled", false, CVar.SERVERONLY);
+
+    /// Automatically starts a map vote when returning to the lobby.
+    /// Requires auto voting to be enabled.
+    public static readonly CVarDef<bool> MapAutoVoteEnabled =
+        CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
+
+    /// Automatically starts a gamemode vote when returning to the lobby.
+    /// Requires auto voting to be enabled.
+    public static readonly CVarDef<bool> PresetAutoVoteEnabled =
+        CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
+
+    #endregion
 }
