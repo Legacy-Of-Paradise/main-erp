54832c13117361e944889bc15c594cb2611e6777
Evgeniy <73418250+Evgencheg@users.noreply.github.com>
2025-07-29 15:27:02 +0300

    Apply suggestions from code review

--- Content.Client/Atmos/Overlays/GasTileOverlay.cs
+++ Content.Client/Atmos/Overlays/GasTileOverlay.cs
@@ -115,7 +115,7 @@ namespace Content.Client.Atmos.Overlays
             for (var i = 0; i < _gasCount; i++)
             {
                 var delays = _frameDelays[i];
-                if (delays == null || delays.Length == 0)
+                if (delays == null || delays.Length == 0) // LOP edit
                     continue;
 
                 var frameCount = _frameCounter[i];
@@ -126,14 +126,16 @@ namespace Content.Client.Atmos.Overlays
                     continue;
 
                 _timer[i] -= time;
+                // LOP edit start
                 if (_frames[i] != null)
                     _frameCounter[i] = (frameCount + 1) % _frames[i].Length;
+                // LOP edit end
             }
 
             for (var i = 0; i < FireStates; i++)
             {
                 var delays = _fireFrameDelays[i];
-                if (delays == null || delays.Length == 0)
+                if (delays == null || delays.Length == 0) // LOP edit
                     continue;
 
                 var frameCount = _fireFrameCounter[i];
@@ -142,9 +144,10 @@ namespace Content.Client.Atmos.Overlays
 
                 if (_fireTimer[i] < time) continue;
                 _fireTimer[i] -= time;
-
+                // LOP edit start
                 if (_fireFrames[i] != null)
                     _fireFrameCounter[i] = (frameCount + 1) % _fireFrames[i].Length;
+                // LOP edit end
             }
         }
 
@@ -168,7 +171,7 @@ namespace Content.Client.Atmos.Overlays
                 xformQuery,
                 _xformSys);
 
-            var mapUid = _mapManager.GetMapEntityId(args.MapId);
+            var mapUid = _mapManager.GetMapEntityId(args.MapId); // LOP edit
 
             if (_entManager.TryGetComponent<MapAtmosphereComponent>(mapUid, out var atmos))
                 DrawMapOverlay(drawHandle, args, mapUid, atmos);
